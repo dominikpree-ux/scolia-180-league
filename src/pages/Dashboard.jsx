8e596ef080a09345901d6758fb9a3b18c047c5b8
Dominik Pree <official.pureuncut@gmail.com>
2026-02-11 23:09:46 +0000

    File changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -17,7 +17,6 @@ import PlayerRecruitmentCard from "../components/dashboard/PlayerRecruitmentCard
 import PlayerProfileCard from "../components/dashboard/PlayerProfileCard";
 import PlayerRequestsCard from "../components/dashboard/PlayerRequestsCard";
 import MyPlayerRequestsCard from "../components/dashboard/MyPlayerRequestsCard";
-import PlayerMessagesCard from "../components/dashboard/PlayerMessagesCard";
 import Chat from "../components/dashboard/Chat";
 import { format } from "date-fns";
 import { de } from "date-fns/locale";
